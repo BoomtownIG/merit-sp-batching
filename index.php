af151f450eefb4b4bad2ce308a28c3cd45ed8f3f
Matt Ross <matt.ross@boomtownig.com>
2014-09-19 19:59:30 +0000

    Add namespace to function names. Add de/activate hooks. change copy

--- index.php
+++ index.php
@@ -11,24 +11,41 @@
 
 defined('ABSPATH') or die();
 
+register_activation_hook( __FILE__, 'sp_activate' );
+function sp_activate() {
+	register_setting('general','sp_host', 'esc_attr');
+	register_setting('general','sp_port', 'esc_attr');
+	register_setting('general','sp_username', 'esc_attr');
+	register_setting('general','sp_password', 'esc_attr');
+	register_setting('general','sp_dfilepath', 'esc_attr');
+}
+register_deactivation_hook( __FILE__, 'sp_deactivate' );
+function sp_deactivate() {
+	unregister_setting('general','sp_host', 'esc_attr');
+	unregister_setting('general','sp_port', 'esc_attr');
+	unregister_setting('general','sp_username', 'esc_attr');
+	unregister_setting('general','sp_password', 'esc_attr');
+	unregister_setting('general','sp_dfilepath', 'esc_attr');
+}
+
 /*****************************************************************************************************
 	Add a options for sp_batching info
 *****************************************************************************************************/
 add_action('admin_init', 'sp_settings');
 function sp_settings() {
 	add_settings_section(
-		'my_settings_section', // Section ID
-		'My Options Title', // Section Title
-		'my_section_options_callback', // Callback
-		'general' // What Page?  This makes the section show up on the General Settings Page
+		'sp_settings_section',
+		'Service Partner Batching Settings',
+		'sp_section_options_callback',
+		'general'
 	);
 
 	add_settings_field(
 		'sp_host',
 		'Hostname',
-		'my_textbox_callback',
+		'sp_textbox_callback',
 		'general',
-		'my_settings_section',
+		'sp_settings_section',
 		array(
 			'sp_host',
 		)
@@ -37,9 +54,9 @@ function sp_settings() {
 	add_settings_field(
 		'sp_port',
 		'Port',
-		'my_number_callback',
+		'sp_number_callback',
 		'general',
-		'my_settings_section',
+		'sp_settings_section',
 		array(
 			'sp_port',
 		)
@@ -48,9 +65,9 @@ function sp_settings() {
 	add_settings_field(
 		'sp_username',
 		'Username',
-		'my_textbox_callback',
+		'sp_textbox_callback',
 		'general',
-		'my_settings_section',
+		'sp_settings_section',
 		array(
 			'sp_username',
 		)
@@ -59,9 +76,9 @@ function sp_settings() {
 	add_settings_field(
 		'sp_password',
 		'Password',
-		'my_password_callback',
+		'sp_password_callback',
 		'general',
-		'my_settings_section',
+		'sp_settings_section',
 		array(
 			'sp_password',
 		)
@@ -70,36 +87,30 @@ function sp_settings() {
 	add_settings_field(
 		'sp_dfilepath',
 		'Destination File Path',
-		'my_textbox_callback',
+		'sp_textbox_callback',
 		'general',
-		'my_settings_section',
+		'sp_settings_section',
 		array(
 			'sp_dfilepath',
 		)
 	);
-
-	register_setting('general','sp_host', 'esc_attr');
-	register_setting('general','sp_port', 'esc_attr');
-	register_setting('general','sp_username', 'esc_attr');
-	register_setting('general','sp_password', 'esc_attr');
-	register_setting('general','sp_dfilepath', 'esc_attr');
 }
 
-function my_section_options_callback() { // Section Callback
+function sp_section_options_callback() { // Section Callback
 	echo '<p>This information is used to upload vetted Service Partners</p>';
 }
 
-function my_password_callback($args) {  // Textbox Callback
+function sp_password_callback($args) {  // Textbox Callback
 	$option = get_option($args[0]);
 	echo '<input type="password" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" class="regular-text" />';
 }
 
-function my_number_callback($args) {  // Number Callback
+function sp_number_callback($args) {  // Number Callback
 	$option = get_option($args[0]);
 	echo '<input type="number" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" class="regular-text" />';
 }
 
-function my_textbox_callback($args) {  // Password Callback
+function sp_textbox_callback($args) {  // Password Callback
 	$option = get_option($args[0]);
 	echo '<input type="text" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" class="regular-text" />';
 }
@@ -107,8 +118,8 @@ function my_textbox_callback($args) {  // Password Callback
 /*****************************************************************************************************
 	Add a checkbox to declare whether or not the Service Partner has been vetted
 *****************************************************************************************************/
-add_action('post_submitbox_start', 'add_sp_vetted_checkbox');
-function add_sp_vetted_checkbox() {
+add_action('post_submitbox_start', 'sp_add_vetted_checkbox');
+function sp_add_vetted_checkbox() {
 	global $post;
 
 	if ($post->post_type !== 'mss_service_partner') {
@@ -120,8 +131,8 @@ function add_sp_vetted_checkbox() {
 /*****************************************************************************************************
 	Handle the checkbox declaring whether or not the Service Partner has been vetted
 *****************************************************************************************************/
-add_action('save_post_mss_service_partner', 'save_vetted_info');
-function save_vetted_info($post_id) {
+add_action('save_post_mss_service_partner', 'sp_save_vetted_info');
+function sp_save_vetted_info($post_id) {
 
 	$sp_vetted = false;
 	if (isset($_REQUEST['sp_vetted'])) {
@@ -133,8 +144,8 @@ function save_vetted_info($post_id) {
 /*****************************************************************************************************
 	Only show Service Partners which have not yet been vetted
 *****************************************************************************************************/
-add_filter('posts_where' , 'mss_service_partner_table_filtering');
-function mss_service_partner_table_filtering($where) {
+add_filter('posts_where' , 'sp_table_filtering');
+function sp_table_filtering($where) {
 	global $wpdb;
 	global $typenow;
 
@@ -150,8 +161,8 @@ function mss_service_partner_table_filtering($where) {
 /*****************************************************************
 	add the button to batch to vetted Service Partners
 *****************************************************************/
-add_filter( 'views_edit-mss_service_partner', 'add_batch_button' );
-function add_batch_button($views) {
+add_filter( 'views_edit-mss_service_partner', 'sp_add_batch_button' );
+function sp_add_batch_button($views) {
 	$views['my-button'] = '<button id="batch-vetted-records" type="button"  title="Batch Vetted Records" style="margin:5px">Batch Vetted Records</button>';
 	return $views;
 }
@@ -159,8 +170,8 @@ function add_batch_button($views) {
 /*****************************************************************
 	move the batch button and hook it to an AJAX endpoint
 *****************************************************************/
-add_action( 'admin_head-edit.php', 'move_batch_button' );
-function move_batch_button() {
+add_action( 'admin_head-edit.php', 'sp_move_batch_button' );
+function sp_move_batch_button() {
 	global $current_screen;
 
 	// Not our post type, exit earlier
@@ -196,8 +207,8 @@ function move_batch_button() {
 /*****************************************************************
 	register AJAX listener for batch endpoint
 *****************************************************************/
-add_action('wp_ajax_batch_service_partners', 'batch_service_partners');
-function batch_service_partners() {
+add_action('wp_ajax_batch_service_partners', 'sp_batch_service_partners');
+function sp_batch_service_partners() {
 
 	header('Content-Type: application/json');
 
